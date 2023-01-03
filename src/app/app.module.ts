5346ab24af094cbb840b6f9feb6fd3a120d5cc33
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-01-03 18:21:33 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AddMemberComponent } from './components/admin/add-member/add-member.component';
+import { EditMemberComponent } from './components/admin/edit-member/edit-member.component';
+import { ShowMembersComponent } from './components/admin/show-members/show-members.component';
+import { AddNewsComponent } from './components/news/add-news/add-news.component';
+import { EditNewsComponent } from './components/news/edit-news/edit-news.component';
+import { DeleteNewsComponent } from './components/news/delete-news/delete-news.component';
+import { DeleteMemberComponent } from './components/admin/delete-member/delete-member.component';
+import { ShowNewsComponent } from './components/news/show-news/show-news.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LogInComponent,
+    SignInComponent,
+    FooterComponent,
+    ShowMembersComponent,
+    AddMemberComponent,
+    EditMemberComponent,
+    DeleteMemberComponent,
+    AddNewsComponent,
+    EditNewsComponent,
+    DeleteNewsComponent,
+    ShowNewsComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/news/show-news/show-news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-news',
+  templateUrl: './show-news.component.html',
+  styleUrls: ['./show-news.component.scss']
+})
+export class ShowNewsComponent {
+
+}
