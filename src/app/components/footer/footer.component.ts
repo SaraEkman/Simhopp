b0c3b5512c5f54eb617c9cd1c9d12660ac6fea17
sara <sara.ekmanMedieinstitutet@gmail.com>
2023-01-15 14:21:46 +0100

    :lipstick: style footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  downloadSportAdmin() {
+    window.open('https://support.sportadmin.se/support/solutions/articles/48000565561-ladda-ner-sportadmins-medlemsapp');
+  }
+  supportMember() {
+    window.open('https://sportadmin.se/form/form.asp?ID=%7B9C1F87E3-2DCE-4AE7-A8DE-7249C480E80D%7D');
+  }
 }
